3e7ccce3b6ce4f8b9ca939be89c96dd1f0a058cb
github username <12ivazquez@gmail.com>
2017-10-23 14:40:28 -0500

    Class level scope methods for Recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,4 +5,12 @@ class Recipe < ApplicationRecord
 
   validates :name, uniqueness: true
   validates :steps, length: { in: 10..2000 }
+
+  def self.short_time
+    Recipe.all.select{ |recipe| recipe.time <= 30 }
+  end
+
+  def self.by_user(user)
+    Recipe.all.select{ |recipe| recipe.user_id = user.id }
+  end
 end
