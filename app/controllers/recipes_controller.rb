44dfbb968ece76e35912ad5c7b48f02e072f05f1
github username <12ivazquez@gmail.com>
2017-11-01 15:19:50 -0500

    Fully implemented master branch features

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -10,7 +10,14 @@ class RecipesController < ApplicationController
   end
 
   def index
-    @recipes = Recipe.all
+    @cuisines = Cuisine.all
+    if !params[:cuisine].blank?
+      @recipes = Recipe.by_cuisine(params[:cuisine])
+    elsif !params[:item].blank?
+      @recipes = Recipe.by_item(params[:item])
+    else
+      @recipes = Recipe.all
+    end
     respond_to do |format|
       format.html { render :index }
       format.json { render json: @recipes, :layout => false }
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,10 +1,9 @@
 class Recipe < ApplicationRecord
   belongs_to :user
   has_many :items, dependent: :destroy
+  belongs_to :cuisine
   accepts_nested_attributes_for :items, reject_if: lambda {|attributes| attributes['name'].blank?},
     allow_destroy: true
-
-  belongs_to :cuisine
 
   validates :name, uniqueness: true
   validates :steps, length: { in: 10..2000 }
